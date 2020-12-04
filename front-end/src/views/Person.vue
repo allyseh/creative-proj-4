86f35bf54bbf1791d91970cc144fa48db0309a24
Allyse Hettinger <51002304+allyseh@users.noreply.github.com>
2020-12-03 18:44:06 -0700

    Update Person.vue

--- front-end/src/views/Person.vue
+++ front-end/src/views/Person.vue
@@ -1,7 +1,15 @@
 <template>
 <div class="people">
   <h1>My People</h1>
-  
+  <div class="display-person" v-for="person in persons" :key="person.id"> 
+      <h2>{{person.name}}</h2>
+      <p><h3>Gender:</h3> {{person.gender}} <h3>Age:</h3> {{person.age}} <h3>Relation:</h3> {{person.relation}} </p>
+      <p><h3>Likes:</h3> {{person.likes}} </p>
+      <div class="buttons">
+        <button @click="toggleEdit(person)">Edit</button>
+        <button @click="deletePerson(person)">Remove</button>
+      </div>
+  </div>
 </div>
 </template>
 
